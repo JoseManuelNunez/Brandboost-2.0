9bc0cd4f913896cab2364e3d11802b5bab57fc88
JoseManuelNunez <josemanuelnunezreyes@gmail.com>
2026-01-16 10:16:46 -0400

    feat: Implement `NegocioLayoutComponent` and `CustomHeader` components for the business module, including styling and a profile image asset.

---
+++ src/app/modules/negocio/components/custom-header/custom-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custom-header',
+  imports: [],
+  templateUrl: './custom-header.html',
+  styleUrl: './custom-header.css',
+})
+export class CustomHeader {
+
+}
--- src/app/modules/negocio/layouts/negocio-layout/negocio-layout.component.ts
+++ src/app/modules/negocio/layouts/negocio-layout/negocio-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CustomSidebarComponent } from '../../components/custom-sidebar/custom-sidebar.component';
+import { CustomHeader } from '../../components/custom-header/custom-header';
 
 @Component({
   selector: 'app-negocio-layout',
   standalone: true,
-  imports: [RouterOutlet, CustomSidebarComponent],
+  imports: [RouterOutlet, CustomSidebarComponent, CustomHeader],
   templateUrl: './negocio-layout.component.html',
   styleUrl: './negocio-layout.component.css'
 })
