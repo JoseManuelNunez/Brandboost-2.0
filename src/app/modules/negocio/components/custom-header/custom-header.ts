5146fc79ef089a7981ddeb5d1f186b3c2d6504e1
JoseManuelNunez <josemanuelnunezreyes@gmail.com>
2026-01-16 14:32:16 -0400

    feat: Add initial layout, custom header, and dashboard components for the negocio module.

--- src/app/modules/negocio/components/custom-header/custom-header.ts
+++ src/app/modules/negocio/components/custom-header/custom-header.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-custom-header',
+  standalone: true,
   imports: [],
   templateUrl: './custom-header.html',
   styleUrl: './custom-header.css',
 })
 export class CustomHeader {
+  @Output() menuClick = new EventEmitter<void>();
 
+  onMenuClick() {
+    this.menuClick.emit();
+  }
 }
--- src/app/modules/negocio/layouts/negocio-layout/negocio-layout.component.ts
+++ src/app/modules/negocio/layouts/negocio-layout/negocio-layout.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { CustomSidebarComponent } from '../../components/custom-sidebar/custom-sidebar.component';
 import { CustomHeader } from '../../components/custom-header/custom-header';
 
 @Component({
   selector: 'app-negocio-layout',
   standalone: true,
-  imports: [RouterOutlet, CustomSidebarComponent, CustomHeader],
+  imports: [RouterOutlet, CustomSidebarComponent, CustomHeader, CommonModule],
   templateUrl: './negocio-layout.component.html',
   styleUrl: './negocio-layout.component.css'
 })
-export class NegocioLayoutComponent { }
+export class NegocioLayoutComponent {
+  sidebarOpen = false;
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+  }
+
+  closeSidebar() {
+    this.sidebarOpen = false;
+  }
+}
